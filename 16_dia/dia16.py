c3e0e1604ac82c2e07ea097a4e985f1cc8069aa6
vmga09 <victor.gonzalez.a@gmail.com>
2023-03-13 10:52:26 -0300

    Update dia16.py

--- 16_dia/dia16.py
+++ 16_dia/dia16.py
@@ -29,3 +29,35 @@ print("date_object =", date_object)
 
 d = date(2022, 1, 1)
 print(d)
+
+today = date(2022, 2, 15)
+new_year = date(2023, 3, 13)
+time_left_for_newyear = str(
+    (new_year - today)).split(',')[0].split(' ')[0]  # Solo en dias
+# Time left for new year:  27 days, 0:00:00
+print('Time left for new year: ', time_left_for_newyear)
+
+
+# 💻 Exercises: Day 16
+# Get the current day, month, year, hour, minute and timestamp from datetime module
+fecha = datetime.now()
+print(fecha)
+
+# Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
+print(fecha.strftime("%m/%d/%Y %H:%M:%S"))
+#     Today is 5 December, 2019. Change this time string to time.
+fecha_string = "5, December, 2019"
+fecha_time = datetime.strptime(fecha_string, "%d, %B, %Y")
+print(fecha_time)
+#     Calculate the time difference between now and new year.
+diff_new_year = date(2024, 1, 1) - date(fecha.year, fecha.month, fecha.day)
+print(diff_new_year)
+#     Calculate the time difference between 1 January 1970 and now.
+diff_new_year = date(fecha.year, fecha.month, fecha.day) - date(1970, 1, 1)
+
+print(diff_new_year)
+#     Think, what can you use the datetime module for ? Examples:
+#     Time series analysis
+#     To get a timestamp of any activities in an application
+#     Adding posts on a blog
+#     🎉 CONGRATULATIONS ! 🎉
